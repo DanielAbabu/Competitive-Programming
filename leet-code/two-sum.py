0e169d6c02d855bb22af4ceaa580f55b0f15af80
DanielAbabu <101533329+DanielAbabu@users.noreply.github.com>
2023-11-28 18:08:03 +0300

    Added solution for Two Sum

---
+++ leet-code/two-sum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nu=set(nums)
+        for i in range(len(nums)):
+            z=target-nums[i]
+            if (z) in nu and i!=nums.index(z):
+                n=nums.index(z)
+                return [i,n]
