e6f7b208eaa1db28869482cd49a21e5e1c7291c3
DanielAbabu <101533329+DanielAbabu@users.noreply.github.com>
2024-01-17 19:32:48 +0300

    Add solution for Subarray Sum Equals K

---
+++ leetcode/subarray-sum-equals-k.py
@@ -0,0 +1,16 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        pre = []
+        dic = defaultdict(int)
+        dic[0] = 1
+        summ = ans = 0
+
+        for i in range(len(nums)):
+            summ += nums[i]
+            pre.append(summ)
+        
+        for i in range(len(pre)):
+            ans += dic.get(pre[i]-k, 0)
+            dic[pre[i]] += 1
+
+        return ans
