d73e71038c4c135cae9c7dd5b6ec6aefd84e95a4
DanielAbabu <101533329+DanielAbabu@users.noreply.github.com>
2024-03-04 19:59:10 +0300

    Add solution for Combination Sum II

---
+++ leetcode/leetcode/combination-sum-ii.py
@@ -0,0 +1,26 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        buck = []
+        ans = []
+        check = set()
+        candidates.sort()
+
+        def backtrack(i):
+            if target == sum(buck):
+                if tuple(buck) not in check:
+                    ans.append(buck[:])
+                    check.add(tuple(buck) )
+                return
+
+            if target < sum(buck):
+                return
+
+            val =None
+            for j in range(i,len(candidates)):
+                if val != candidates[j]:
+                    buck.append( candidates[j] )
+                    backtrack(j+1)
+                    val = buck.pop()       
+        
+        backtrack(0)
+        return ans
