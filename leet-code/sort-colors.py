4aa095b1672a4b54f6001ecbb1d263cfdc5d8a35
DanielAbabu <101533329+DanielAbabu@users.noreply.github.com>
2024-06-12 12:27:19 +0300

    Added solution for Sort Colors

--- leet-code/sort-colors.py
+++ leet-code/sort-colors.py
@@ -5,7 +5,7 @@ class Solution:
         """
         for i in range(len(nums)):
             for j in range(len(nums)-1):
-                if nums[j]>nums[j+1]:
-                    nums[j],nums[j+1] = nums[j+1],nums[j]
+                if nums[j] > nums[j+1]:
+                    nums[j] , nums[j+1] = nums[j+1] , nums[j]
         
             
